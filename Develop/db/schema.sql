ed06c59933705722ab4730fed15f1f4d247b9f69
Andrew Ahler <andrewahler@gmail.com>
2021-01-16 13:12:00 -0600

    corrected schema

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,16 +1,11 @@
 -- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
-SHOW {DATABASES | SCHEMAS}
-    [LIKE 'pattern' | WHERE expr],
+
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
-CREATE {DATABASE | SCHEMA} [IF NOT EXISTS] db_name
-    [create_option] ...
 
-create_option: [DEFAULT] {
-    CHARACTER SET [=] charset_name
-  | COLLATE [=] collation_name
-}
+
+
 
 
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,20 +1,32 @@
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
-const Tag = require('./Tag');
+const Tags = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey:"category_id"
+})
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey:"category_id"
+})
 
 // Products belongToMany Tags (through ProductTag)
+Tags.belongsTo(Product,{
+  foreignKey:"tag_id"
+})
 
 // Tags belongToMany Products (through ProductTag)
+Product.belongsTo(Tags,{
+  foreignKey:"category_id"
+})
 
 module.exports = {
   Product,
   Category,
-  Tag,
+  Tags,
   ProductTag,
 };
