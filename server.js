f409908b827e517988281838997fce73e25c45f2
Andrew Ahler <andrewahler@gmail.com>
2021-01-19 14:09:42 -0600

    updated server.js to original

--- server.js
+++ server.js
@@ -1,22 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-const { Sequelize } = require('sequelize');
 // import sequelize connection
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'andrewahler@gmail.com',
-  password : 'Vandy2021!'
-});
- 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
- 
-  console.log('connected as id ' + connection.threadId);
-});
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -24,10 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-sequelize.close()
-// sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: true })
 
+// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
